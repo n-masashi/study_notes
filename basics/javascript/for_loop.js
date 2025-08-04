c99ea1b5eefe8e31c47349d22223dc24760cbea2
Nicosuke <m.masashi@miyata-tech.net>
2025-08-04 21:44:18 +0900

    Create for_loop.js

---
+++ basics/javascript/for_loop.js
@@ -0,0 +1,19 @@
+// for文
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// 配列の繰り返し
+let arr = [10, 20, 30];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// while文
+let j = 0;
+while (j < 3) {
+    console.log(j);
+    j++;
+}
+
+// Tips: for文はインデックス制御がしやすいので便利！
